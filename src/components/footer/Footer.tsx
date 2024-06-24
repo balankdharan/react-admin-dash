4073870379d7a57cd88554bff2d956621027ce17
balankdharan <balankdharan@gmail.com>
2024-06-24 23:46:43 +0530

    intial setup

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./footer.scss";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>admin</span>
+      <span>@ copyright admin dashboard</span>
+    </div>
+  );
+};
+
+export default Footer;
